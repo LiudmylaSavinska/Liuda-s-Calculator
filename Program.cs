1616023d5b643d16e5d2a3c35d47df874bfa93bb
LiudmylaSavinska <110248943+LiudmylaSavinska@users.noreply.github.com>
2024-07-11 18:11:28 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Arithmetic operations");
+
+Console.WriteLine("Please, enter the first number");
+var number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please, enter the second number");
+var number2 = Convert.ToDouble(Console.ReadLine());
+
+var Sum1 = number1 + number2;
+Console.WriteLine($"Sum: {number1} + {number2} = {Sum1}");
+var Sum2 = number1 - number2;
+Console.WriteLine($"Sum: {number1} - {number2} = {Sum2}");
+var Sum3 = number1 * number2;
+Console.WriteLine($"Sum: {number1} * {number2} = {Sum3}");
+var Sum4 = number1 / number2;
+Console.WriteLine($"Sum: {number1} / {number2} = {Sum4}");
